8abfa4bccfe7caf9913423978aa223d521197854
Teow Hua Jun <huajun@Teows-MacBook-Pro.local>
2022-09-16 01:13:59 +0800

    feat: sum endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import echoRouter from './echo'
+import sumRouter from './sum'
 
 const router = Router()
 router.use('/', echoRouter)
+router.use('/', sumRouter)
 
 export default router
---
+++ src/routes/sum.ts
@@ -0,0 +1,27 @@
+import { Request, Response, Router } from 'express'
+import { celebrate, Joi, Segments } from 'celebrate'
+import { asyncErrorWrapper } from '../errors'
+
+const router = Router()
+
+const echoCelebrate = {
+  [Segments.BODY]: Joi.object({
+    a: Joi.number().required(),
+    b: Joi.number().required(),
+  }),
+}
+
+type sumBody = {
+  a: number
+  b: number
+}
+
+const sum = async (req: Request, res: Response) => {
+  const { a, b }: sumBody = req.body
+  const sum = a + b
+  return res.status(200).json({ sum })
+}
+
+router.post('/sum', celebrate(echoCelebrate), asyncErrorWrapper(sum))
+
+export default router
