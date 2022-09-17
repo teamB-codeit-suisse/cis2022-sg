67783a577e6cb955863772a472bf801127cea210
Teow Hua Jun <huajun@Teows-MacBook-Pro.local>
2022-09-18 05:11:34 +0800

    test

--- src/solvers/connect4.ts
+++ src/solvers/connect4.ts
@@ -8,7 +8,7 @@ async function connect4Solution(battleId: string): Promise<void> {
 
   const columns = 'ABCDEFG'
   const game = new (Connect4.Connect4AI as any)()
-  game.recursiveDepthLimit = 5
+  game.recursiveDepthLimit = 4
 
   let myToken = ''
 
