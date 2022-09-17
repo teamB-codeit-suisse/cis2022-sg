34e9ecf72013c4b0e5ec313ca03da91d5b1ea767
Teow Hua Jun <huajun@Teows-MacBook-Pro.local>
2022-09-17 16:27:44 +0800

    test

---
+++ src/routes/stonks.route.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Request, Response, Router } from 'express'
+import { celebrate, Joi, Segments } from 'celebrate'
+import { asyncErrorWrapper } from '../utils/errors'
+
+const router = Router()
+
+const echoCelebrate = {
+  [Segments.BODY]: Joi.object({
+    energy: Joi.number().required(),
+    capital: Joi.number().required(),
+    timeline: Joi.object().required(),
+  }),
+}
+
+type stocks = {
+  price: number
+  qty: number
+}
+
+type stonksBody = {
+  energy: number
+  capital: number
+  timeline: Record<string, Record<string, stocks>>
+}
+
+const stonksHandler = async (req: Request, res: Response) => {
+  const { energy, capital, timeline }: stonksBody = req.body
+  return res.status(200).json({ message: 'helloworld' })
+}
+
+router.post('/sum', celebrate(echoCelebrate), asyncErrorWrapper(stonksHandler))
+
+export default router
