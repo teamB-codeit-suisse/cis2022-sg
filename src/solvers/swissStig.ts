3263f8126c770629dade3a379215872f6fece2b5
caando <zhangjikun907475@gmail.com>
2022-09-17 18:49:00 +0800

    feat: swissStig route

---
+++ src/solvers/swissStig.ts
@@ -0,0 +1,23 @@
+type Question = {
+  lower: number,
+  higher: number
+}
+
+type InterviewPart1 = {
+  questions: Question[],
+  maxRating: number
+}
+
+type InterviewPart2 = {
+  questions: Question[],
+  maxRating: number
+}
+
+export function swissStigPart1Solution (input: InterviewPart1[]) {
+  input
+
+}
+
+export function swissStigPart2Solution(input: InterviewPart2[]) {
+  input
+}
