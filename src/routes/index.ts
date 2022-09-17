7992166075d593a7134ecce43bbe02bf3601e4d6
An Jun <limanjun99@gmail.com>
2022-09-17 12:03:08 +0800

    fix: merge conflict

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,11 +4,8 @@ import sumRouter from './sum.route'
 import tickerStreamRouter from './tickerStream.route'
 import cryptocollapzRouter from './cryptocollapz.route'
 import calendarDaysRouter from './calendarDays.route'
-<<<<<<< HEAD
 import rubiksRouter from './rubiks.route'
-=======
 import travellingSuisseRobot from './travellingSuisseRobot.route'
->>>>>>> a41c72b (feat: travelling suisse robot)
 
 const router = Router()
 router.use('/', echoRouter)
@@ -16,10 +13,7 @@ router.use('/', sumRouter)
 router.use(tickerStreamRouter)
 router.use(cryptocollapzRouter)
 router.use(calendarDaysRouter)
-<<<<<<< HEAD
 router.use(rubiksRouter)
-=======
 router.use(travellingSuisseRobot)
->>>>>>> a41c72b (feat: travelling suisse robot)
 
 export default router
