801e9faf22769f183c583ad525645a382b98ed28
An Jun <limanjun99@gmail.com>
2022-09-17 21:03:32 +0800

    feat: connect 4 testing

---
+++ src/solvers/connect4.ts
@@ -0,0 +1,45 @@
+import axios from 'axios'
+
+export function connect4Solution(battleId: string) {
+  const src = `https://cis2022-arena.herokuapp.com/connect4/start/${battleId}`
+  const evtSource = new EventSource(src)
+
+  let myToken = ''
+
+  const postMove = (column: string) => {
+    axios.post(src, {
+      action: 'putToken',
+      column,
+    })
+  }
+
+  type Message = {
+    youAre: string
+    id: string
+    player: string
+    action: string
+    column: string
+    winner: string
+  }
+  evtSource.onmessage = (event: MessageEvent<Message>) => {
+    const data = event.data
+    if (data.hasOwnProperty('youAre')) {
+      // initial event
+      myToken = data['youAre']
+      postMove('A')
+    } else if (data.hasOwnProperty('player')) {
+      if (data.action === 'putToken') {
+        if (data.player === myToken) return
+        else {
+          // someone flip table
+          /* do nothing */
+        }
+      }
+    } else {
+      console.log(
+        data.winner === myToken ? 'I won!' : data.winner === 'draw' ? 'draw' : 'I lost ):'
+      )
+      evtSource.close()
+    }
+  }
+}
