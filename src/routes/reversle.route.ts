cb3a5284d0d808d374d9469afef55f2e2aa27bc5
An Jun <limanjun99@gmail.com>
2022-09-18 00:40:03 +0800

    fix: debug

--- src/routes/reversle.route.ts
+++ src/routes/reversle.route.ts
@@ -1,22 +1,33 @@
 import { Request, Response, Router } from 'express'
+import { reverslePart1Solution, reverslePart2Solution } from 'solvers/reversle'
 import { asyncErrorWrapper } from '../utils/errors'
 
 const router = Router()
 
 type reversleBody = {
   equationLength: number
-  attemptsAllowed?: number
-  attemptsLeft?: number
-  equationHistory?: string[]
-  resultHistory?: string[]
+  attemptsAllowed: number
+  attemptsLeft: number
+  equationHistory: string[][]
+  resultHistory: string[][]
 }
 
 const reversle = async (req: Request, res: Response) => {
-  const { equationLength }: reversleBody = req.body
-  console.log('GIVEN INPUT')
-  console.log(req.body)
-  const equation: string[] = ['0', '=']
-  for (let i = 2; i < equationLength; i++) equation.push('0')
+  const {
+    equationLength,
+    attemptsAllowed,
+    equationHistory,
+    resultHistory,
+    attemptsLeft,
+  }: reversleBody = req.body
+  let equation: string[]
+  if (attemptsAllowed) {
+    // initial query
+    equation = reverslePart1Solution(equationLength, attemptsAllowed)
+  } else {
+    // subsequent queries
+    equation = reverslePart2Solution(equationLength, equationHistory, resultHistory, attemptsLeft)
+  }
   return res.status(200).json({ equation })
 }
 
