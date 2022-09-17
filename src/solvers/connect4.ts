e292be83ecad5cdb821cb06ac9cad258234933ba
An Jun <limanjun99@gmail.com>
2022-09-18 04:13:56 +0800

    fix

--- src/solvers/connect4.ts
+++ src/solvers/connect4.ts
@@ -1,34 +1,13 @@
 import axios from 'axios'
 import https from 'https'
+import Connect4 from 'connect4-ai'
 
 export async function connect4Solution(battleId: string): Promise<void> {
   const src = `https://cis2022-arena.herokuapp.com/connect4/play/${battleId}`
   const evtSrc = `https://cis2022-arena.herokuapp.com/connect4/start/${battleId}`
 
-  const board: number[][] = Array(6)
-    .fill(0)
-    .map((_) => Array(7).fill(0))
   const columns = 'ABCDEFG'
-  const addMoveToBoard = (column: string, player: number) => {
-    const c = columns.indexOf(column)
-    let placed = false
-    for (let i = 5; !placed && i >= 0; i--) {
-      if (board[i][c] !== 0) continue
-      board[i][c] = player
-      placed = true
-    }
-    return placed
-  }
-  /*
-  const removeFromBoard = (column: string) => {
-    const c = columns.indexOf(column)
-    for (let i = 0; i < 6; i++) {
-      if (board[i][c] === 0) continue
-      board[i][c] = 0
-      return
-    }
-  }
-  */
+  const game = new (Connect4.Connect4AI as any)()
 
   let myToken = ''
 
@@ -55,33 +34,20 @@ export async function connect4Solution(battleId: string): Promise<void> {
             // initial event
             myToken = data['youAre']
             if (myToken === '🔴') {
-              addMoveToBoard('D', 1)
-              postMove('D')
+              game.playAI('hard')
+              postMove(columns[game.plays[game.plays.length - 1]])
             }
           } else if (action) {
             if (data.action === 'putToken') {
               if (data.player !== myToken) {
-                const valid = addMoveToBoard(data.column, -1)
+                const valid = game.canPlay(columns.indexOf(data.column))
                 if (!valid) {
                   if (timeout !== undefined) clearTimeout(timeout)
                   flipTable()
                 } else {
-                  let done = false
-                  for (let i = 0; i < 100; i++) {
-                    const column = columns[Math.floor(Math.random() * 7)]
-                    if (!addMoveToBoard(column, 1)) continue
-                    postMove(column)
-                    done = true
-                    break
-                  }
-                  if (!done) {
-                    for (const col of columns) {
-                      if (addMoveToBoard(col, 1)) {
-                        postMove(col)
-                        break
-                      }
-                    }
-                  }
+                  game.play(columns.indexOf(data.column))
+                  game.playAI('hard')
+                  postMove(columns[game.plays[game.plays.length - 1]])
                 }
               } else {
                 // my move
